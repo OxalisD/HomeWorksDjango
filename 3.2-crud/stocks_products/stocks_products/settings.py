269966d9b3e91871a0d1a4a01b9c7e6f1c5a91c7
Kislitsa Danila <danilakislitsa@gmail.com>
2023-09-27 12:43:12 +0700

    Сделал домашку 3.2

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -81,8 +81,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
+        'USER': 'postgresql',
+        'PASSWORD': 'postgresql',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+
     }
 }
 
@@ -129,3 +132,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
